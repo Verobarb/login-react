a452e5b74e02dc8819260b59be702f46bc938064
Veronica <nicole_veronica@hotmail.com>
2024-02-17 17:30:53 -0500

    formulario de registro basico, sin completar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.svg'; // Import your SVG image
 import './App.css';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-   
-    </div>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Nombres</Form.Label>
+        <Form.Control type="text" placeholder="Escribe Nombres" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicLastname">
+        <Form.Label>Apellidos</Form.Label>
+        <Form.Control type="text" placeholder="Escribe Apellidos" />
+     </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Correo</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label></Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Escribe contraseña" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
 
-
 export default App;
-
-
